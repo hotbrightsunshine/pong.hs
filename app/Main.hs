38689c31d9ec9c78bd4150fc587ff23f925fb49b
Francesco <7frapar@gmail.com>
2022-07-16 19:38:17 +0200

    continuing

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Graphics.Gloss
+import Graphics.Gloss.Data.ViewPort
 
 window :: Display
 window = InWindow "Pong" (w, h) (offset, offset) 
@@ -9,13 +10,63 @@ window = InWindow "Pong" (w, h) (offset, offset)
           offset = 1
 
 background :: Color
-background = white
+background =    black
 
-drawing :: Picture
-drawing = pictures [  color circleColor $ circleSolid 10
-                    , color rectColor $ rectangleSolid 20 80 ]
-          where circleColor = light red
-                rectColor = dark blue
+
+data Pong = Game {
+    ballPos ::  (Float, Float)
+,   ballVel ::  (Float, Float)
+,   player1 ::  Float
+,   player2 ::  Float
+} deriving Show
+
+
+initial :: Pong
+initial = Game {
+    ballPos =   (0, 0)
+,   ballVel =   (1, -3)
+,   player1 =   0
+,   player2 =   0
+}
+
+
+render :: Pong -> Picture
+render  game =  pictures    [ ball, walls,
+                            paddle1, paddle2 ]
+                where 
+                    -- Ball
+                    ball = translate    (fst (ballPos game))
+                                        (snd (ballPos game))
+                                        $ color red 
+                                        $ circleSolid 10
+
+                    -- Walls
+                    wall :: Float -> Picture 
+                    wall offset =       color white 
+                                        $ translate 0 (offset) 
+                                        $ rectangleSolid 400 10
+                    
+                    walls = pictures [ wall (-150), wall 150 ]
+
+                    -- Paddles
+                    makePaddle :: Color -> Float -> Float -> Picture 
+                    makePaddle col x offset = translate x offset 
+                                              $ color col $ rectangleSolid 20 80
+
+                    paddle1 = makePaddle blue (-225) (player1 game) 
+                    paddle2 = makePaddle blue 225 (player2 game) 
+
+moveBall :: Float -> Pong -> Pong 
+moveBall time game = game { ballPos = (x', y') }
+                     where 
+                        (x, y) = ballPos game
+                        (vx, vy) = ballVel game
+
+                        x' = x + vx * time
+                        y' = y + vy * time
 
 main :: IO ()
-main = display window background drawing
+main = simulate background 60 initial render update
+    where 
+        update :: ViewPort -> Float -> Pong -> Pong
+        update _ = moveBall
