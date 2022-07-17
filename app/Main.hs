6a2b5a1e01d30c2a56a02bdff3c88ba1ab130316
Francesco <7frapar@gmail.com>
2022-07-17 21:44:20 +0200

    (+) added wallCollided and wallBounce

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import Graphics.Gloss
 import Graphics.Gloss.Data.ViewPort
 
+
 window :: Display
 window = InWindow "Pong" (w, h) (offset, offset) 
     where h = 300
@@ -35,15 +36,14 @@ render  game =  pictures    [ ball, walls,
                             paddle1, paddle2 ]
                 where 
                     -- Ball
-                    ball = translate    (fst (ballPos game))
-                                        (snd (ballPos game))
+                    ball =  uncurry     translate (ballPos game)
                                         $ color red 
                                         $ circleSolid 10
 
                     -- Walls
                     wall :: Float -> Picture 
                     wall offset =       color white 
-                                        $ translate 0 (offset) 
+                                        $ translate 0 offset
                                         $ rectangleSolid 400 10
                     
                     walls = pictures [ wall (-150), wall 150 ]
@@ -65,8 +65,24 @@ moveBall time game = game { ballPos = (x', y') }
                         x' = x + vx * time
                         y' = y + vy * time
 
+
+wallCollision :: Float -> (Float, Float) -> Bool
+wallCollision rad pos = upper || lower
+    where
+        upper = (snd pos) + rad >= 150 - (10 / 2)
+        lower = (snd pos) - rad <= (-150) + (10 / 2)
+
+
+wallCollided :: Pong -> Pong
+wallCollided game = game { ballVel vx, vy' }
+    where
+        (vx, vy) = ballVel game
+        vy' = if wallCollision (ballLoc game) radius
+                 then -vy
+                 else vy
+
 main :: IO ()
-main = simulate background 60 initial render update
+main = simulate window background 60 initial render update
     where 
         update :: ViewPort -> Float -> Pong -> Pong
         update _ = moveBall
